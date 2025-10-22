aa4548154437a3063212a5416467c6e8e3dea45b
James-Nyangada <jamesedwin03@gmail.com>
2025-10-22 09:44:46 +0300

    send mail fix

--- src/utilities/sendEmail.js
+++ src/utilities/sendEmail.js
@@ -1,11 +1,17 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587, // use 587 for TLS
+  secure: false, // true for port 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // allows self-signed certs, prevents TLS hang
+  },
+  connectionTimeout: 10000, // 10 seconds
 });
 
 module.exports = async (to, code) => {
